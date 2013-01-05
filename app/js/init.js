18392b753a2bcc62c44cafe0584f1e82b7b66acd
Rafael <rafaelkyrdan@gmail.com>
2013-01-05 20:55:03 +0200

    add bigInteger library
    add bigInteger library

--- app/js/init.js
+++ app/js/init.js
@@ -67,3 +67,19 @@ win.on( 'loaded', function() {
 function w ( a,b,c,d ) {
      console.log( a,b,c,d );
 }
+
+
+//var crypto = require("crypto");
+//var assert = require("assert");
+
+var hash1 = require("crypto").createHash('sha1').update('Test123').digest('hex');
+var hash2 = require("crypto").createHash('md5').update('Test123').digest('binary');
+var hash3 = require("crypto").createHash('md5').update(hash1, 'hex');
+
+require("assert").equal(hash1, '8308651804facb7b9af8ffc53a33a22d6a1c8ac2', 'Test SHA1');
+require("assert").equal(hash2, 'h\u00ea\u00cb\u0097\u00d8o\fF!\u00fa+\u000e\u0017\u00ca' +
+    '\u00bd\u008c', 'Test MD5 as binary');
+
+w("h1", hash1);
+w("h2", hash2);
+w('h3', hash3);
